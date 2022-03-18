31e735770333b1cc1e6aa785562582b3d12c4396
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-18 16:18:40 +0100

    Created header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Home Page')
+
+@section('content')
+<main class="bg-dark">
+    <div class="container">
+  
+    </div>
+</main>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,20 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset(css/app.css) }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>My Train || @yield('title')</title>
 </head>
 
 <body>
     <header>
-        <nav class="navbar navbar-light bg-light">
+        <nav class="navbar navbar-light bg-danger">
             <div class="container">
               <a class="navbar-brand" href="{{route('home')}}">
-                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glassdoor.com%2FInterview%2FTrenitalia-Interview-Questions-E1380720.htm&psig=AOvVaw1E8Oy070cGimWTQDZVi8aS&ust=1647702134527000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLivm_X2z_YCFQAAAAAdAAAAABAD" alt="" width="30" height="24">
+                <img  src="https://www.begear.it/wp-content/uploads/2015/11/Logo-trenitalia.jpg" alt="" width="30" height="24">
               </a>
             </div>
           </nav>
     </header>
     
+    
+    @yield('content')
+    
+
 </body>
 </html>¨
